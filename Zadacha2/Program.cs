a4b4d1048c1c071b4068a190e153d12d56a9c4de
Nadya <Nadi-lv@yandex.ru>
2023-01-15 13:21:30 +0400

    Max and min

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Определение большего и меньшего числа");
+
+Console.WriteLine ("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if (number2 > max)
+{
+    max = number2;
+    Console.WriteLine("максимальное число " + max);
+    Console.WriteLine("минимальное число " + number1);
+}
+else if (number2 == max)
+{
+     Console.WriteLine("числа равны");
+}
+else 
+{
+    Console.WriteLine("максимальное число " + max);
+    Console.WriteLine("минимальное число " + number2);
+}
