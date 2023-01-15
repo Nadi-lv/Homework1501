53cb24c87053ad283f34e4ae49a329c422220a37
Nadya <Nadi-lv@yandex.ru>
2023-01-15 16:37:25 +0400

    Chetnie ot 1 do n

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Четные числа от 1 до n");
+
+Console.WriteLine ("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 0) 
+{
+    Console.WriteLine( " введите другое число");
+}
+
+int i = 2;
+
+ while (i < number +1)
+{    
+    if (( i % 2 ) == 0)
+    Console.Write ( i + ", ");
+    i = i + 2;
+}
