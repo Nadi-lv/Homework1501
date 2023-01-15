5102b5c0eade550a414af424b0f421648eb5746f
Nadya <Nadi-lv@yandex.ru>
2023-01-15 16:27:23 +0400

    Chetnoe chislo

---
+++ Zadacha6/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Четное ли число?");
+
+Console.WriteLine ("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 0) 
+{
+    Console.WriteLine( " введите другое число");
+}
+
+if ((number % 2) == 0) 
+{
+    Console.WriteLine( number + " - четное число ");
+}
+else 
+{
+    Console.WriteLine( number + " - нечетное число ");
+}
